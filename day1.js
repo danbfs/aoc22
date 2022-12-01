369fa9bc88d37c11657c34b8fc6f24908b17e325
Daniel Baldi <danielbaldifs@gmail.com>
2022-12-01 08:17:47 -0300

    Create day1.js

---
+++ day1.js
@@ -0,0 +1,15 @@
+const data = input.split("\n\n").map((item) => item.split("\n"));
+
+const getCaloriesAsNumber = (input) =>
+  input.reduce((acc, curr) => Number(acc) + Number(curr), 0);
+
+const totalCalories = data.map((item) => getCaloriesAsNumber(item));
+
+const sorted = totalCalories.sort((a, b) => a - b);
+
+const totalElves = sorted.length - 1;
+
+const firstAnswer = sorted[totalElves];
+
+const secondAnswer =
+  sorted[totalElves] + sorted[totalElves - 1] + sorted[totalElves - 2];
